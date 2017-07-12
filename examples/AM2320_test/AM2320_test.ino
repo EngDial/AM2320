973c109404b50f1b5bb96630b3b166895153a994
Aleksey <ra9mig@gmail.com>
2017-07-12 19:56:54 +0600

    Add files via upload

---
+++ examples/AM2320_test/AM2320_test.ino
@@ -0,0 +1,31 @@
+#include <Wire.h>
+#include <AM2320.h>
+
+AM2320 th;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+}
+
+void loop() {
+  Serial.println("Chip = AM2320");
+  switch(th.Read()) {
+    case 2:
+      Serial.println("  CRC failed");
+      break;
+    case 1:
+      Serial.println("  Sensor offline");
+      break;
+    case 0:
+      Serial.print("  Humidity = ");
+      Serial.print(th.Humidity);
+      Serial.println("%");
+      Serial.print("  Temperature = ");
+      Serial.print(th.cTemp);
+      Serial.println("*C");
+      Serial.println();
+      break;
+  }
+  delay(2000);
+}
