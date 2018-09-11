72d00caf3c460ff4e58f8afa7c259bc7fad111c2
Aleksey <ra9mig@gmail.com>
2018-09-11 15:29:51 +0600

    Update AM2320.cpp

--- src/AM2320.cpp
+++ src/AM2320.cpp
@@ -25,11 +25,11 @@ uint8_t i;
 // ---=== Constructor ===---
 AM2320::AM2320(TwoWire* com_wire)
 {
-  communication = comwire;
+  communication = com_wire;
 }
 
 // ---=== Start convertion ===--
-int AM2320::startConvert()
+uint8_t AM2320::startConvert()
 {
   State = 1;
   // Wake
@@ -44,36 +44,32 @@ int AM2320::startConvert()
   return State;	
 }
 
-int AM2320::getData()
+uint8_t AM2320::getData()
 {
-byte buf[8];
+uint8_t buf[8];
 uint8_t i; 
-unsigned int crc_temp;
+uint16_t crc_temp;
 //------------------------------
-  // clr buf
-  for (i = 0; i < 8; i++) 
-    buf[i] = 0x00; 
   State = 2;
+  for (i = 0; i < 8; i++) buf[i] = 0x00; 
   // Read data (8 byte = (2 Header)+(2 Hum)+(2 Temp)+(2 CRC16))
   communication->requestFrom(AM2320_address, 8); 
-  for (i = 0; i < 8; i++) 
-    buf[i] = communication->read();
-
+  for (i = 0; i < 8; i++) buf[i] = communication->read();
   // Check CRC16
   crc_temp = buf[7] << 8;
   crc_temp += buf[6];
   if (crc_temp == CRC16(buf, 6)) 
   {
     State = 0;
-    cTemp = ( ((buf[4] & 0x7F) << 8) + buf[5] ) / 10.0;
-    cTemp = (buf[4] & 0x80) ? cTemp * (-1) : cTemp;
-    Humidity = ( (buf[2] << 8) + buf[3] ) / 10.0;
+    cTemp = (((buf[4] & 0x7F) << 8) + buf[5]) / 10.0;
+    cTemp = (buf[4] & 0x80) ? -cTemp : cTemp;
+    Humidity = ((buf[2] << 8) + buf[3]) / 10.0;
   }
   return State;
 }
 
 // Read
-int AM2320::Read()
+uint8_t AM2320::Read()
 {
   if (startConvert() != 0) return State;
   delay(2); // delay 2 ms
